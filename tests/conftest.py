93de5174dfb081f889b439d2637a33c4cda7bf61
mr-raccoon-97 <eric.m.cardozo@mi.unc.edu.ar>
2024-11-08 21:58:54 +0000

    Add workflows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,14 @@
 from os import path
 from shutil import rmtree
 from pytest import fixture
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 @fixture(scope='session')
 def directory():
     yield 'data/test'
-    rmtree('data')
-
-    
+    try:
+        rmtree('data')
+    except PermissionError:
+        logger.warning('Could not remove data directory')
